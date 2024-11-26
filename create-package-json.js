04e268b8c8542865a16b60f75b97533755b8f4d9
khai96_ <hvksmr1996@gmail.com>
2024-11-26 15:37:41 +0700

    use `pnpm add`

--- create-package-json.js
+++ create-package-json.js
@@ -5,9 +5,6 @@ const object = {
   name: 'test',
   private: true,
   version: '0.0.0',
-  dependencies: {
-    'is-number': '7.0.0',
-  },
 }
 
 const json = JSON.stringify(object, undefined, 2) + '\n'
--- reproduce.bash
+++ reproduce.bash
@@ -6,7 +6,7 @@ echo 'Reproducing...' >&2
 pretty-exec -- mkdir -p work
 pretty-exec -- node create-package-json.js
 pretty-exec -- node create-npmrc.js
-pretty-exec -- run-at work pnpm install
+pretty-exec -- run-at work pnpm add is-number@7.0.0
 pretty-exec -- run-at work pnpm patch is-number
 pretty-exec -- bat -pp 'work/node_modules/.pnpm_patches/is-number@7.0.0/index.js'
 pretty-exec -- bash -c 'echo "// DEFENITELY EDITED" > work/node_modules/.pnpm_patches/is-number@7.0.0/index.js'
